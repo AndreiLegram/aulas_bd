3382fe7a1f4745753075e070fa041d4335c64883
Andrei Legramante <andrei.050103@bento.local>
2024-12-05 22:04:31 -0300

    trabalho_3

--- trabalho_3/andrei-trabalho-3.sql
+++ trabalho_3/andrei-trabalho-3.sql
@@ -1,5 +1,206 @@
-/* Trabalho 3 - X */
+/* Trabalho 3 - Triggers, Loops, Cursores e Events no MySQL */
 
 USE campeonatobrasileiro;
 
-/*  */
+SET GLOBAL event_scheduler = ON;
+
+/* A. Criar um gatilho chamado novo_gol_partida, que será executado após a ação
+de inserção na tabela de gols. O gatilho em questão irá somar um gol ao
+placar do time que fez o gol. Atente para a possibilidade do gol ser contra,
+onde o gol deverá ir para o outro time, e não para o time do jogador. */
+DROP TRIGGER IF EXISTS novo_gol_partida;
+
+DELIMITER $$
+
+CREATE TRIGGER novo_gol_partida AFTER INSERT ON gols
+FOR EACH ROW
+BEGIN
+    IF NEW.gol_contra = 1 THEN
+        IF EXISTS (
+            SELECT 1 
+            FROM partidas 
+            WHERE id = NEW.id_partida 
+              AND id_time_mandante = (SELECT id_time FROM jogadores WHERE id = NEW.id_jogador)
+        ) THEN
+            UPDATE partidas
+            SET placar_time_visitante = placar_time_visitante + 1
+            WHERE id = NEW.id_partida;
+        ELSE
+            UPDATE partidas
+            SET placar_time_mandante = placar_time_mandante + 1
+            WHERE id = NEW.id_partida;
+        END IF;
+    ELSE
+        IF EXISTS (
+            SELECT 1 
+            FROM partidas 
+            WHERE id = NEW.id_partida 
+              AND id_time_mandante = (SELECT id_time FROM jogadores WHERE id = NEW.id_jogador)
+        ) THEN
+            UPDATE partidas
+            SET placar_time_mandante = placar_time_mandante + 1
+            WHERE id = NEW.id_partida;
+        ELSE
+            UPDATE partidas
+            SET placar_time_visitante = placar_time_visitante + 1
+            WHERE id = NEW.id_partida;
+        END IF;
+    END IF;
+END $$
+
+DELIMITER ;
+
+/* B. Crie uma consulta SQL que dispare o gatilho criado acima. */
+INSERT INTO gols (id_partida, id_jogador, tempo, gol_contra)
+VALUES (8, 61, 90, 1);
+
+/* C. Criar uma trigger chamada gol_anulado_partida, que será executada após
+uma ação de delete na tabela de gols. Ela irá subtrair um gol ao placar do
+time que fez o gol. Atente para a possibilidade do gol ser contra, onde o gol
+deverá ser subtraído do placar do outro time, e não para o time do jogador. */
+DROP TRIGGER IF EXISTS gol_anulado_partida;
+
+DELIMITER $$
+
+CREATE TRIGGER gol_anulado_partida AFTER DELETE ON gols
+FOR EACH ROW
+BEGIN
+    IF OLD.gol_contra = 1 THEN
+        IF EXISTS (
+            SELECT 1 
+            FROM partidas 
+            WHERE id = OLD.id_partida 
+              AND id_time_mandante = (SELECT id_time FROM jogadores WHERE id = OLD.id_jogador)
+        ) THEN
+            UPDATE partidas
+            SET placar_time_visitante = placar_time_visitante - 1
+            WHERE id = OLD.id_partida;
+        ELSE
+            UPDATE partidas
+            SET placar_time_mandante = placar_time_mandante - 1
+            WHERE id = OLD.id_partida;
+        END IF;
+    ELSE
+        IF EXISTS (
+            SELECT 1 
+            FROM partidas 
+            WHERE id = OLD.id_partida 
+              AND id_time_mandante = (SELECT id_time FROM jogadores WHERE id = OLD.id_jogador)
+        ) THEN
+            UPDATE partidas
+            SET placar_time_mandante = placar_time_mandante - 1
+            WHERE id = OLD.id_partida;
+        ELSE
+            UPDATE partidas
+            SET placar_time_visitante = placar_time_visitante - 1
+            WHERE id = OLD.id_partida;
+        END IF;
+    END IF;
+END $$
+
+DELIMITER ;
+
+/* D. Crie uma consulta SQL que dispare a trigger criado acima. */
+DELETE FROM gols WHERE id = 12;
+
+/* E. Criar uma tabela chamada transferencias, contendo os seguintes campos: id,
+id_time_antigo, id_time_novo e id_jogador. Atente para a criação das chaves
+estrangeiras. */
+DROP TABLE IF EXISTS transferencias;
+
+CREATE TABLE transferencias (
+	id int(11) NOT NULL AUTO_INCREMENT,
+	id_time_antigo int(11) NOT NULL,
+	id_time_novo int(11) NOT NULL,
+	id_jogador int(11) NOT NULL,
+	PRIMARY KEY (id),
+ 	FOREIGN KEY (id_time_antigo) REFERENCES times (id), 
+ 	FOREIGN KEY (id_time_novo) REFERENCES times (id)
+);
+
+/* F. Criar um gatilho chamado transferir_jogador, que será executado após a ação
+de update na tabela de jogadores. O gatilho em questão irá verificar se o id
+do time do jogador após a atualização é diferente do id do time do jogador
+antes da atualização e, neste caso, inserir um registro na tabela
+transferencias criada acima. */
+DROP TRIGGER IF EXISTS transferir_jogador;
+
+DELIMITER $$
+
+CREATE TRIGGER transferir_jogador AFTER UPDATE ON jogadores
+FOR EACH ROW
+BEGIN
+    IF OLD.id_time != NEW.id_time THEN
+        INSERT INTO transferencias (id_time_antigo, id_time_novo, id_jogador)
+        VALUES (OLD.id_time, NEW.id_time, NEW.id);
+    END IF;
+END $$
+
+DELIMITER ;
+
+/* G. Crie uma consulta SQL que dispare o gatilho criado acima. */
+UPDATE jogadores SET id_time = 3 WHERE id = 10;
+
+/* H. Criar uma trigger chamada validar_gol, que verifica se o tempo do gol estiver
+entre 0 e 90. Caso não esteja, um erro de SQL deverá ser lançado. A trigger
+deverá ser disparada antes da inserção de gols. */
+DROP TRIGGER IF EXISTS validar_gol;
+
+DELIMITER $$
+
+CREATE TRIGGER validar_gol BEFORE INSERT ON gols
+FOR EACH ROW
+BEGIN
+	IF NEW.tempo < 0 THEN
+		SIGNAL SQLSTATE '22006' SET message_text = 'O tempo do gol deve ser maior que 0';
+	END IF;
+   	IF NEW.tempo > 90 THEN
+		SIGNAL SQLSTATE '22006' SET message_text = 'O tempo do gol deve ser menor que 90';
+	END IF;
+END $$
+
+DELIMITER ;
+
+/* I. Crie uma consulta SQL que dispare a trigger acima. */
+INSERT INTO gols (id_partida, id_jogador, tempo, gol_contra)
+VALUES (8, 61, 91, 1);
+
+/* J. Criar uma coluna SQL na tabela jogadores chamada quantidade_gols, que
+deverá ser do tipo inteiro. */
+ALTER TABLE jogadores ADD COLUMN quantidade_gols int(11);
+
+/* K. Criar um evento SQL que irá executar a cada minuto, calcular a quantidade
+de gols feitas por cada jogador, e atualizar o valor da coluna quantidade_gols. */
+DROP EVENT IF EXISTS atualizar_quantidade_gols;
+
+DELIMITER $$
+
+CREATE EVENT atualizar_quantidade_gols
+ON SCHEDULE EVERY 1 MINUTE
+DO
+BEGIN
+    UPDATE jogadores
+    SET quantidade_gols = (
+        SELECT COUNT(*)
+        FROM gols
+        WHERE gols.id_jogador = jogadores.id AND gols.gol_contra = 0
+    );
+END $$
+
+DELIMITER ;
+
+/* L. Criar um event SQL chamado limpar_transferencias, que será executado no
+dia 06/12/2024, às 22h. O evento deverá excluir todos os registros da tabela
+de transferências. */
+DROP EVENT IF EXISTS limpar_transferencias;
+
+DELIMITER $$
+
+CREATE EVENT limpar_transferencias
+ON SCHEDULE AT '2024-12-06 22:00:00'
+DO
+BEGIN
+    DELETE FROM transferencias;
+END $$
+
+DELIMITER ;
--- trabalho_3/campeonatobrasileiro.sql
+++ trabalho_3/campeonatobrasileiro.sql
@@ -1,3 +1,49 @@
 CREATE DATABASE campeonatobrasileiro;
 
 USE campeonatobrasileiro;
+
+CREATE TABLE times (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nome varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO times (id, nome) VALUES (1, 'Grêmio'), (2, 'Internacional'), (3, 'Flamengo'), (4, 'Fluminense'), (5, 'São Paulo'), (6, 'Palmeiras'), (7, 'Bragantino'), (8, 'Athletico');
+
+CREATE TABLE jogadores (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nome varchar(255) NOT NULL,
+  posicao enum('G','Z','L','M','A') NOT NULL,
+  id_time int(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_time) REFERENCES times (id)
+);
+
+INSERT INTO jogadores (id, nome, posicao, id_time) VALUES (1, 'Marchesín', 'G', 1), (2, 'João Pedro', 'L', 1), (3, 'Jemerson', 'Z', 1), (4, 'Rodrigo Ely', 'Z', 1), (5, 'Reinaldo', 'L', 1), (6, 'Villasanti', 'M', 1), (7, 'Dodi', 'M', 1), (8, 'Monsalve', 'M', 1), (9, 'Cristaldo', 'M', 1), (10, 'Pavón', 'A', 1), (11, 'Soteldo', 'A', 1), (12, 'Braithwaite', 'A', 1), (13, 'Rochet', 'G', 2), (14, 'Nathan', 'L', 2), (15, 'Mercado', 'Z', 2), (16, 'Vitão', 'Z', 2), (17, 'Renê', 'L', 2), (18, 'Fernando', 'M', 2), (19, 'Thiago Maia', 'M', 2), (20, 'Alan Patrick', 'M', 2), (21, 'Bruno Tabata', 'M', 2), (22, 'Wanderson', 'A', 2), (23, 'Borré', 'A', 2), (24, 'Valencia', 'A', 2), (25, 'Rossi', 'G', 3), (26, 'Varela', 'L', 3), (27, 'David Luiz', 'Z', 3), (28, 'Léo Pereira', 'Z', 3), (29, 'Ayrton Lucas', 'L', 3), (30, 'De La Cruz', 'M', 3), (31, 'Gérson', 'M', 3), (32, 'Alcaraz', 'M', 3), (33, 'Arrascaeta', 'M', 3), (34, 'Éverton Cebolinha', 'A', 3), (35, 'Gabigol', 'A', 3), (36, 'Pedro', 'A', 3), (37, 'Fábio', 'G', 4), (38, 'Samuel Xavier', 'L', 4), (39, 'Thiago Silva', 'Z', 4), (40, 'Felipe Melo', 'Z', 4), (41, 'Marcelo', 'L', 4), (42, 'Renato Augusto', 'M', 4), (43, 'Martinelli', 'M', 4), (44, 'Ganso', 'M', 4), (45, 'Arias', 'M', 4), (46, 'Keno', 'A', 4), (47, 'Cano', 'A', 4), (48, 'John Kennedy', 'A', 4), (49, 'Rafael', 'G', 5), (50, 'Rafinha', 'L', 5), (51, 'Arboleda', 'Z', 5), (52, 'Alan Franco', 'Z', 5), (53, 'Patryck', 'L', 5), (54, 'Pablo Maia', 'M', 5), (55, 'Rodrigo Nestor', 'M', 5), (56, 'Alisson', 'M', 5), (57, 'Galoppo', 'M', 5), (58, 'Lucas Moura', 'A', 5), (59, 'Calleri', 'A', 5), (60, 'Luciano', 'A', 5), (61, 'Weverton', 'G', 6), (62, 'Marcos Rocha', 'L', 6), (63, 'Murilo', 'Z', 6), (64, 'Gómez', 'Z', 6), (65, 'Piquierez', 'L', 6), (66, 'Aníbal Moreno', 'M', 6), (67, 'Richard Ríos', 'M', 6), (68, 'Rafael Veiga', 'M', 6), (69, 'Dudu', 'A', 6), (70, 'Felipe Anderson', 'A', 6), (71, 'Rony', 'A', 6), (72, 'Estêvão', 'A', 6), (73, 'Cleiton', 'G', 7), (74, 'Hurtado', 'L', 7), (75, 'Pedro Henrique', 'Z', 7), (76, 'Realpe', 'Z', 7), (77, 'Luan Cândido', 'L', 7), (78, 'Juninho Capixaba', 'L', 7), (79, 'Matheus Fernandes', 'M', 7), (80, 'Lucas Evangelista', 'M', 7), (81, 'Lincoln', 'M', 7), (82, 'Jhon Jhon', 'A', 7), (83, 'Vitinho', 'A', 7), (84, 'Borbas', 'A', 7), (85, 'Léo Linck', 'G', 8), (86, 'Godoy', 'L', 8), (87, 'Thiago Heleno', 'Z', 8), (88, 'Kaíque Rocha', 'Z', 8), (89, 'Esquivel', 'L', 8), (90, 'Fernandinho', 'M', 8), (91, 'Christian', 'M', 8), (92, 'Zapelli', 'M', 8), (93, 'Cuello', 'A', 8), (94, 'Canobbio', 'A', 8), (95, 'Mastriani', 'A', 8), (96, 'Pablo', 'A', 8);
+
+CREATE TABLE partidas (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  id_time_mandante int(11) NOT NULL,
+  id_time_visitante int(11) NOT NULL,
+  datahora datetime NOT NULL,
+  placar_time_mandante int(11) NOT NULL,
+  placar_time_visitante int(11) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_time_mandante) REFERENCES times (id), 
+  FOREIGN KEY (id_time_visitante) REFERENCES times (id)
+);
+
+INSERT INTO partidas (id, id_time_mandante, id_time_visitante, datahora, placar_time_mandante, placar_time_visitante) VALUES (1, 1, 2, '2024-10-13 16:00:00', 2, 0), (2, 3, 4, '2024-10-13 16:00:00', 1, 1), (3, 5, 6, '2024-10-13 16:00:00', 0, 0), (4, 7, 8, '2024-10-13 16:00:00', 1, 0), (5, 1, 8, '2024-10-20 16:00:00', 2, 0), (6, 7, 2, '2024-10-20 16:00:00', 1, 0), (7, 3, 5, '2024-10-20 16:00:00', 0, 3), (8, 6, 4, '2024-10-20 16:00:00', 0, 0);
+
+CREATE TABLE gols (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  id_partida int(11) NOT NULL,
+  id_jogador int(11) NOT NULL,
+  tempo int(11) NOT NULL,
+  gol_contra tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (id), 
+  FOREIGN KEY (id_partida) REFERENCES partidas (id),
+  FOREIGN KEY (id_jogador) REFERENCES jogadores (id)
+);
+
+INSERT INTO gols (id, id_partida, id_jogador, tempo, gol_contra) VALUES (1, 1, 9, 30, 0), (2, 1, 12, 60, 0), (3, 2, 36, 36, 0), (4, 2, 47, 47, 0), (5, 4, 75, 10, 0), (6, 5, 10, 80, 0), (7, 5, 12, 85, 0), (8, 6, 16, 50, 1), (9, 7, 56, 30, 0), (10, 7, 59, 60, 0), (11, 7, 59, 90, 0);
